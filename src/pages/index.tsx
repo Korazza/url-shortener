e7af624ffbd76afb8584e3e773ba8ea985c4270d
Korazza <corattiroberto@gmail.com>
2023-04-06 20:57:24 +0200

    chore: better link appearance

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,20 +31,20 @@ const HomePage: NextPage = () => {
           <h1 className="text-4xl font-extrabold tracking-tight lg:text-6xl">
             Url Shortener
           </h1>
-          <div className="flex w-full place-items-center justify-center gap-2">
+          <div className="flex w-fit place-items-center justify-center gap-2 rounded-lg bg-slate-900/75 px-3 py-2 text-slate-50 shadow">
             <input
               ref={urlInput}
-              className="rounded bg-slate-900/75 px-3 py-2 text-slate-50 shadow outline-none"
+              className="rounded bg-slate-900/75 py-2 text-center outline-none placeholder:text-slate-400"
               placeholder="https://..."
               onKeyDown={(e) => {
                 if (e.key === "Enter") mutateShortUrl();
               }}
             />
-            <span className="text-slate-400">/to/</span>
+            <span className="text-slate-300">/to/</span>
             <input
               ref={slugInput}
               maxLength={Number(env.NEXT_PUBLIC_SLUG_MAX_LENGTH)}
-              className="w-28 rounded bg-slate-900/75 py-2 text-center text-slate-50 shadow outline-none"
+              className="w-28 rounded bg-slate-900/75 py-2 text-center outline-none placeholder:text-slate-400"
               placeholder="slug..."
               onKeyDown={(e) => {
                 if (e.key === "Enter") mutateShortUrl();
@@ -52,16 +52,63 @@ const HomePage: NextPage = () => {
             />
           </div>
           <button
-            className="rounded-full bg-blue-900 px-5 py-2 shadow-lg transition hover:bg-blue-900/75 active:bg-blue-900/50"
+            className="rounded-full bg-blue-900 px-5 py-2 text-xl shadow-lg transition hover:bg-blue-900/75 active:bg-blue-900/50"
             onClick={mutateShortUrl}
           >
             Generate
           </button>
-          {isLoading && <span className="text-slate-400">Loading...</span>}
+          {isLoading && (
+            <div>
+              <svg
+                className="mr-2 h-8 w-8 animate-spin fill-blue-800/75 text-gray-200 dark:text-slate-800/75"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill"
+                />
+              </svg>
+              <span className="sr-only">Loading...</span>
+            </div>
+          )}
           {isSuccess && (
-            <Link href={`/to/${data.slug}`} target="_blank">
-              {window.location.origin}/to/{data.slug}
-            </Link>
+            <div className="group flex place-items-center gap-4 rounded-xl bg-slate-600/75 px-10 py-5 text-slate-50 shadow-lg">
+              <Link
+                className="text-xl hover:text-sky-600"
+                href={`/to/${data.slug}`}
+                target="_blank"
+              >
+                <code>
+                  {window.location.origin}/to/{data.slug}
+                </code>
+              </Link>
+              <button
+                className="hidden  rounded-md bg-slate-800/75 p-1 text-slate-400 transition hover:bg-slate-800 hover:text-slate-300 active:text-slate-400  group-hover:inline"
+                type="button"
+                onClick={() => {
+                  void navigator.clipboard.writeText(
+                    `${window.location.origin}/to/${data.slug}`
+                  );
+                }}
+              >
+                <svg
+                  aria-hidden="true"
+                  className="mx-auto h-6 w-6"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M7 9a2 2 0 012-2h6a2 2 0 012 2v6a2 2 0 01-2 2H9a2 2 0 01-2-2V9z"></path>
+                  <path d="M5 3a2 2 0 00-2 2v6a2 2 0 002 2V5h8a2 2 0 00-2-2H5z"></path>
+                </svg>
+              </button>
+            </div>
           )}
           {isError && (
             <span className="text-red-500">
